a938c4a09c499403a094b634bf38ff1d06fb7242
kaileemorrison <77708024+kaileemorrison@users.noreply.github.com>
2021-02-05 08:36:21 -0500

    added comments to bash script

--- MyCount.sh
+++ MyCount.sh
@@ -1,11 +1,11 @@
-for Count in {1..20}
-do 
-if [ $Count -lt 10 ]
+for Count in {1..20} # sets the number of iterations 
+do # begins the for loop
+if [ $Count -lt 10 ] # defines a possible condition for each iteration
 then
-echo "$Count is a single-digit number"
-elif [ $Count -gt 9 ]
+echo "$Count is a single-digit number" # defines what will print out with each iteration
+elif [ $Count -gt 9 ] # defines an alterantive condition for each iteration
 then
-echo "$Count is a double-digit number"
-fi
-done
+echo "$Count is a double-digit number" # defines what will print out with each alternative iteration
+fi # closes the if statment
+done # closes the for loop
 
